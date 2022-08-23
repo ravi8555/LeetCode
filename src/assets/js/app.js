e52fe118055a004ccd31fba81c320b3e62dcfc19
ravi8555 <ravi8555@gmail.com>
2022-08-23 23:53:51 +0530

    initiat commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,15 @@
+import style from '../css/main.scss';
+// import style1 from '../css/main.css';
+// import component from './component.js';
+// import componentCircle from './circle.js'
+import nmlcss from "../css/normalize.scss";
+// leetcode
+import leetCode from "../js/leetcode.js";
+// const heading = "Welcome to Digiserve";
+// document.querySelector('h2').innerText = heading 
+
+console.log("check");
+// cube
+document.body.append(leetCode())
+
+// circle
